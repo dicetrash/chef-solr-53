85b2ed045f1023270571de342b74f2f287d5c06a
dacross <dacross@ea.com>
2015-11-14 00:57:04 -0500

    Versioning was important to the script

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,8 +6,8 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-remote_file "/tmp/solr.tgz" do
-  source "http://apache.mirrors.tds.net/lucene/solr/5.3.1/solr-5.3.1.tgz"
+remote_file "/tmp/solr-#{node['solr']['version']}.tgz" do
+  source "http://apache.mirrors.tds.net/lucene/solr/#{node['solr']['version']}/solr-#{node['solr']['version']}.tgz"
 end
 
 #tarball_x "/tmp/solr.tar.gz" do
@@ -19,10 +19,10 @@ end
 
 execute "solr_untar" do
   cwd "/tmp"
-  command "tar xzf solr.tgz solr-5.3.1/bin/install_solr_service.sh --strip-components=2"
+  command "tar xzf solr-#{node['solr']['version']}.tgz solr-#{node['solr']['version']}/bin/install_solr_service.sh --strip-components=2"
 end
 
 execute "solr_instal" do
   cwd "/tmp"
-  command "./install_solr_service.sh solr.tgz"
+  command "./install_solr_service.sh solr-#{node['solr']['version']}.tgz"
 end
