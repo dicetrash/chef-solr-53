0b10d45da17d52e2725b12509677d377338e6576
dacross <dacross@ea.com>
2015-11-14 01:05:03 -0500

    gaurd it all

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,27 +2,20 @@
 # Cookbook Name:: chef-solr-53
 # Recipe:: default
 #
-# Copyright 2015, YOUR_COMPANY_NAME
+# Free and open source
 #
-# All rights reserved - Do Not Redistribute
-#
-remote_file "/tmp/solr-#{node['solr']['version']}.tgz" do
-  source "http://apache.mirrors.tds.net/lucene/solr/#{node['solr']['version']}/solr-#{node['solr']['version']}.tgz"
-end
-
-#tarball_x "/tmp/solr.tar.gz" do
-#  destination "/tmp/solr"
-#  owner 'root'
-#  group 'root'
-#  action :extract
-#end
-
-execute "solr_untar" do
-  cwd "/tmp"
-  command "tar xzf solr-#{node['solr']['version']}.tgz solr-#{node['solr']['version']}/bin/install_solr_service.sh --strip-components=2"
-end
-
-execute "solr_instal" do
-  cwd "/tmp"
-  command "./install_solr_service.sh solr-#{node['solr']['version']}.tgz"
+unless Dir.exist? "/opt/solr" do 
+	remote_file "/tmp/solr-#{node['solr']['version']}.tgz" do
+		source "http://apache.mirrors.tds.net/lucene/solr/#{node['solr']['version']}/solr-#{node['solr']['version']}.tgz"
+	end
+	
+	execute "solr_untar" do
+		cwd "/tmp"
+		command "tar xzf solr-#{node['solr']['version']}.tgz solr-#{node['solr']['version']}/bin/install_solr_service.sh --strip-components=2"
+	end
+	
+	execute "solr_instal" do
+		cwd "/tmp"
+		command "./install_solr_service.sh solr-#{node['solr']['version']}.tgz"
+	end
 end
