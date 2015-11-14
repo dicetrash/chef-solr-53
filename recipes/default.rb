b8d9917637fa2a33ea2f13efa58843dcecbf1065
dacross <dacross@ea.com>
2015-11-14 00:36:46 -0500

    basically just all bash

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,23 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+remote_file "/tmp/solr.tar.gz" do
+  source "http://apache.mirrors.tds.net/lucene/solr/5.3.1/solr-5.3.1.tgz"
+end
+
+#tarball_x "/tmp/solr.tar.gz" do
+#  destination "/tmp/solr"
+#  owner 'root'
+#  group 'root'
+#  action :extract
+#end
+
+execute "solr_untar" do
+  cwd "/tmp"
+  command "tar xzf solr-5.3.1.tgz solr-5.3.1/bin/install_solr_service.sh --strip-components=2"
+end
+
+execute "solr_instal" do
+  cwd "/tmp"
+  command "./install_solr_service.sh solr-5.3.1.tgz"
+end
